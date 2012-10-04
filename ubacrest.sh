437e943e06bab1cff1f9aa5ce9c022bccfca14a8
Marcus Bergman <marcus.bergman@netlight.se>
2012-10-04 14:44:56 +0200

    Added debug functionality for easy debug printout

--- ubacrest.sh
+++ ubacrest.sh
@@ -3,6 +3,36 @@
 # IMPORT GLOBAL CONFIG FROM ubacrest.cfg
 source ubacrest.cfg
 
-echo ${SRC_BASE_PATH}
+function main() {
+  
+  # Test debug functionality
+  dbg "Hello debug world"
+
+}
+
+# DEBUG: Print variables from config file (manually specified) 
+if [ ${PRINT_DBG} -eq 1 ]; then 
+  echo ${SRC_BASE_PATH}
+  echo ${DST_BASE_PATH}
+  echo ${SRC_BASE_PATH}
+fi
 
 
+# Function to print debug information when $PRINT_DBG is set to 1
+function dbg() {
+  if [ ${PRINT_DBG} -eq 1 ]; then echo $#; fi
+}
+
+function check_backup_folder() {
+  
+  if [ -d $DST_BASE_PATH ]; then
+    return    
+  fi
+  
+}
+
+# CALL MAIN FUNCTION 
+#
+# Call last in script so that all other functions are defined!
+
+main
